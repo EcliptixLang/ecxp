174547304a127e696de5157f389976bedc7848f4
SkyOPG <nicknameopg1@gmail.com>
2025-03-18 17:38:35 +0000

    cleaning: removed useless files

--- src/build.js
+++
@@ -1,7 +0,0 @@
-import { glob } from "glob";
-import { writeFileSync } from "fs";
-
-const globa = await glob("./**/**/**/*.cpp");
-console.log(globa.join(" "))
-
-writeFileSync("hi", `g++ ${globa.join(" ")} -o hi.out`);
