02b4228dbdb400460fae608eb4442a2e3f1b6538
SkyOPG <nicknameopg1@gmail.com>
2025-03-18 17:35:44 +0000

    removed compiler (temporary)

--- src/compiler/expressions/number.cpp
+++
@@ -1,9 +0,0 @@
-#include "../compiler.hpp"
-
-using Nodes = AST::Nodes; 
-using string = std::string;
-
-void Compiler::CNumber(std::shared_ptr<AST::ExprAST>& astNode){
-	AST::NumberExpr* number = dynamic_cast<AST::NumberExpr*>(astNode.get());
-	code += number->Value;
-}
--- src/compiler/expressions/string.cpp
+++
@@ -1,9 +0,0 @@
-#include "../compiler.hpp"
-
-using Nodes = AST::Nodes; 
-using string = std::string;
-
-void Compiler::CString(std::shared_ptr<AST::ExprAST>& astNode){
-	AST::StringExpr* str = dynamic_cast<AST::StringExpr*>(astNode.get());
-	code += "\"" + str->Value + "\"";
-}
